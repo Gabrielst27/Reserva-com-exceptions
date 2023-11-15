b6f1b7a3877d9353b8512c910e14a19e4c063d3b
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-15 15:52:26 -0300

    Criada atualização da data de reserva (s1)

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,24 @@ public class Program {
 		} else {
 			Reservation reservation = new Reservation(roomNumber, checkin, checkout);
 			System.out.println(reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkin = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkout = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			if(checkin.before(now) || checkout.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			} else if(!checkout.after(checkin)){
+				System.out.print("Error ir reservation: Check-out date must be after check-in date.");
+			} else {
+				reservation.updateDate(checkin, checkout);
+				System.out.println(reservation);
+			}
+			
 		}
 		
 		sc.close();
