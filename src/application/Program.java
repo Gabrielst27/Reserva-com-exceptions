63984abb3305a71dbe5849cee3e84f31256dcb3f
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-15 14:33:07 -0300

    Construida Reservation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.text.ParseException;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParseException {
+		
+	
+		
 	}
 
 }
--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -1,6 +1,55 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
 	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	public Reservation() {
+		
+	}
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+	
+	public long duration() {
+		long diff = checkout.getTime() - checkin.getTime();
+		//diff recebe os milissegundos de checkout substraídos pelos milissegundos de checkin
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+		//Converte os milissegundos de diff para dias e os retorna
+	}
+	
+	public void updateDate(Date checkin, Date checkout) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Reservation: Room %d, check-in: %s, checkout: %s, %d nights",
+				roomNumber, sdf.format(checkin), sdf.format(checkout), duration());
+	}
 	
 }
