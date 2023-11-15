3a867894649bebd26f3694fa6c7214dbbb6a509b
Gabriel Silva Torres <gabrielst328@gmail.com>
2023-11-15 16:34:32 -0300

    Criada classe exception personalizada

---
+++ src/exceptions/DomainException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class DomainException extends Exception {
+	private static final long serialVersionUID = 1L; // Versão da classe serializable
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+	
+}
